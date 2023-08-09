d8597a02f3f74072837a2accf760fb38aaea7363
Isaranwa <pennyisaranwa@gmail.com>
2023-08-09 17:03:01 +0300

    Add function getDictionary

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function playSound() {
+  sound.play();
+}
+
 function getDictionary(response) {
   console.log(response);
   let word = document.querySelector("#input").value;
@@ -6,39 +10,36 @@ function getDictionary(response) {
     <div class="word">
 
       <h5>${word}</h5>
+      <button onclick="playSound()">
       <i class="fa-solid fa-volume-high"></i>
+      </button>
       <p>${response.data[0].phonetics[1].text}</p>
       <h3>${response.data[0].meanings[0].partOfSpeech}</h3>
-      <p>
+      
       <ul>
        <li> <i>
           ${response.data[0].meanings[0].definitions[0].definition}
-        </i></li> <br/>
-        <li><i>${response.data[0].meanings[0].definitions[1].definition}</i></li>
+        </i></li>
+        </ul>
+        <h3>${response.data[0].meanings[1].partOfSpeech}</h3>
         <ul>
-      </p>
-      <h3>Synonyms</h3>
-      <p>
-        <i>
-          ${response.data[0]}
-        </i>
-      </p>
-      <hr />
-      <h4>similar sounding words</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-        pellentesque mattis convallis.
-      </p>
+        <li> <i>
+          ${response.data[0].meanings[1].definitions[0].definition}
+        </i></li>
+        </ul>
+      
     </div>
   `;
+  sound.setAttribute("src", `${response.data[0].phonetics[1].audio}`);
+  console.log(sound);
 }
 
 function getWord(event) {
   event.preventDefault();
   let word = document.querySelector("#input").value;
-  console.log(word);
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/ ${word}`;
   axios.get(apiUrl).then(getDictionary);
 }
 let button = document.querySelector("#btn");
 button.addEventListener("click", getWord);
+let sound = document.querySelector("#sound");
